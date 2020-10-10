372afb1490cb2c8a0ce25cebc0b545a06e1134b0
tinchovictory <martin@victory.com.ar>
2020-10-10 15:51:10 -0300

    update github action

--- scripts/run_deploy.js
+++ scripts/run_deploy.js
@@ -1,34 +1,23 @@
 const core = require('@actions/core');
-const { GitHub, context } = require('@actions/github');
+const { getOctokit, context } = require('@actions/github');
 const fs = require('fs');
 
-const run = async () => {
-  console.log('running ci deploy');
-  console.log(context.repo);
+const createRelease = async () => {
   const { owner: currentOwner, repo: currentRepo } = context.repo;
-  console.log(`owner: ${currentOwner}, repo: ${currentOwner}`);
-
-  const tag = '1.0.0';
+  const tag = '1.1.0';
   const releaseName = `Release v${tag}`;
-  const body = '#v1.0.0\n##First Release\n-hello world';
-  const draft = false;
-  const prerelease = false;
+  const body = '# v1.1.0\n## First Release\n- hello world';
   const commitish = context.sha;
 
   try {
     // Github token
-    // const githubToken = core.getInput('githubToken');
     const githubToken = process.env.GITHUB_TOKEN;
 
-    console.log(`github token is ${githubToken}`);
-
-    // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-    const github = new GitHub(githubToken);
+    // Get authenticated GitHub client
+    const github = getOctokit(githubToken);
 
     // Create a release
-    // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
-    // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
-    const createReleaseResponse = await github.repos.createRelease({
+    await github.repos.createRelease({
       owner: currentOwner,
       repo: currentRepo,
       tag_name: tag,
@@ -38,24 +27,15 @@ const run = async () => {
       prerelease: false,
       target_commitish: commitish
     });
-
-    // Get the ID, html_url, and upload URL for the created Release from the response
-    const {
-      data: { id: releaseId, html_url: htmlUrl, upload_url: uploadUrl }
-    } = createReleaseResponse;
-
-    console.log(`Release created!`);
-    console.log(`Release id: ${releaseId}`);
-    console.log(`Release url: ${htmlUrl}`);
-    console.log(`Release upload url: ${uploadUrl}`);
-
-    console.log(createReleaseResponse);
-
-
-    console.log('Done!');
   } catch(error) {
     core.setFailed(error.message);
   }
+}
+
+const run = async () => {
+  console.log('Running ci deploy');
+  await createRelease();
+  console.log('Done!');
 }
 
 run();
