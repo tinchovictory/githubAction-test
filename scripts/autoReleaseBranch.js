a99c85923de76758ea2ff075e68eabf8bba6f171
tinchovictory <martin@victory.com.ar>
2020-10-10 21:21:50 -0300

    create branch

--- scripts/autoReleaseBranch.js
+++ scripts/autoReleaseBranch.js
@@ -34,31 +34,39 @@ const createPullRequest = async () => {
   } catch(error) {
     core.setFailed(error.message);
   }
-}
+};
 
+const createReleaseBranch = async (octo, owner, repo, version) => {
+  const baseBranchSha = await getBranchSha(octo, owner, repo, 'main');
+  const createBranchResponse = await octo.git.createRef({
+    owner,
+    repo,
+    ref: `refs/heads/auto-release/${version}`,
+    sha: baseBranchSha,
+  });
 
-const main = async () => {
-  const githubToken = process.env.GITHUB_TOKEN;
+  console.log(createBranchResponse);
+};
 
-  // Get authenticated GitHub client
-  const octo = getOctokit(githubToken);
+// const main = async () => {
+//   const githubToken = process.env.GITHUB_TOKEN;
 
-  const files = ['test/file.md'];
-  const { owner: currentOwner, repo: currentRepo } = context.repo;
+//   // Get authenticated GitHub client
+//   const octo = getOctokit(githubToken);
+
+//   const files = ['test/file.md'];
+//   const { owner: currentOwner, repo: currentRepo } = context.repo;
   
-  await uploadToRepo(octo, files, currentOwner, currentRepo, 'release/1.0');
-}
+//   await uploadToRepo(octo, files, currentOwner, currentRepo, 'release/1.0');
+// }
 
 const uploadToRepo = async (octo, files, owner, repo, branch) => {
   // gets commit's AND its tree's SHA
   const currentCommit = await getCurrentCommit(octo, owner, repo, branch);
-  console.log(`currentCommit ${currentCommit}`);
-  // const filesPaths = await glob(coursePath)
+  
   const filesPaths = files.map(fullPath => `../${fullPath}`);
   const filesBlobs = await Promise.all(filesPaths.map(createBlobForFile(octo, owner, repo)));
-  console.log(filesBlobs);
-  // const pathsForBlobs = files.map(fullPath => path.relative('../test', fullPath));
-  // console.log(`pathsForBlobs ${pathsForBlobs}`);
+  
   const newTree = await createNewTree(
     octo,
     owner,
@@ -67,8 +75,8 @@ const uploadToRepo = async (octo, files, owner, repo, branch) => {
     files,
     currentCommit.treeSha
   );
-  console.log(`newTree ${newTree}`);
-  const commitMessage = `My commit message`
+  
+  const commitMessage = `Auto-release`;
   const newCommit = await createNewCommit(
     octo,
     owner,
@@ -77,62 +85,70 @@ const uploadToRepo = async (octo, files, owner, repo, branch) => {
     newTree.sha,
     currentCommit.commitSha
   );
-  console.log(`new Commit ${newCommit}`);
+  
   await setBranchToCommit(octo, owner, repo, branch, newCommit.sha);
 };
 
-
-const getCurrentCommit = async (octo, owner, repo, branch) => {
+const getBranchSha = async (octo, owner, repo, branch) => {
   const { data: refData } = await octo.git.getRef({
     owner,
     repo,
     ref: `heads/${branch}`,
   });
-  console.log(`refData: ${refData}`);
-  const commitSha = refData.object.sha;
-  console.log(`commitSha: ${commitSha}`);
+  return refData.object.sha;
+};
+
+const getCurrentCommit = async (octo, owner, repo, branch) => {
+  // const { data: refData } = await octo.git.getRef({
+  //   owner,
+  //   repo,
+  //   ref: `heads/${branch}`,
+  // });
+  
+  const commitSha = await getBranchSha(octo, owner, repo, branch);
+  
   const { data: commitData } = await octo.git.getCommit({
     owner,
     repo,
     commit_sha: commitSha,
   });
-  console.log(`commitData: ${commitData}`);
+  
   return {
     commitSha,
     treeSha: commitData.tree.sha,
   }
 };
 
-// Notice that readFile's utf8 is typed differently from Github's utf-8
 const getFileAsUTF8 = (filePath) => readFileSync(filePath, 'utf-8');
 
 const createBlobForFile = (octo, owner, repo) => async (filePath) => {
   const content = getFileAsUTF8(filePath);
-  console.log(`File content ${content}`);
+  
   const blobData = await octo.git.createBlob({
     owner,
     repo,
     content,
     encoding: 'utf-8',
   });
-  console.log(`blobdata ${blobData}`);
+  
   return blobData.data;
 };
 
 const createNewTree = async (octo, owner, repo, blobs, paths, parentTreeSha) => {
-  // My custom config. Could be taken as parameters
   const tree = blobs.map(({ sha }, index) => ({
     path: paths[index],
     mode: `100644`,
     type: `blob`,
     sha,
   }));
+  
   const { data } = await octo.git.createTree({
     owner,
     repo,
     tree,
     base_tree: parentTreeSha,
   });
+ 
   return data
 };
 
@@ -168,22 +184,26 @@ const setBranchToCommit = (
 
 
 const run = async () => {
-  console.log('Running ci deploy');
-  // await createPullRequest();
+  console.log('Running auto-release...');
 
-  // const coursePath = '../';
-  // const filesPaths = await glob(coursePath);
-  // const filesPaths = ['../test/file.md'];
-  // const pathsForBlobs = filesPaths.map(fullPath => path.relative(coursePath, fullPath));
+  // Bump package.json
 
-  // console.log(`filePath: ${filesPaths}`);
-  // console.log(`pathsForBlobs: ${pathsForBlobs}`);
-
+  // Remove unpublished
   writeFileSync('../test/file.md', '# Cambiado');
+  
+  // Get authenticated GitHub client
+  const githubToken = process.env.GITHUB_TOKEN;
+  const octo = getOctokit(githubToken);
 
-  await main();
+  // Create release branch
+  await createReleaseBranch(octo, owner, repo, '1.1');
 
-  console.log('Done!');
+  const files = ['test/file.md'];
+  const { owner, repo } = context.repo;
+  
+  await uploadToRepo(octo, files, owner, repo, 'release/1.0');
+
+  console.log('\nDone!');
 }
 
 run();
