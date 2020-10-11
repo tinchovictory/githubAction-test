57e0612f6078e9added23b2898cc22496460acc2
tinchovictory <martin@victory.com.ar>
2020-10-11 19:40:16 -0300

    rerun

--- scripts/run_deploy.js
+++ scripts/run_deploy.js
@@ -7,6 +7,7 @@ const {
   currentRepo
  } = require('./githubHelper');
 const { packageVersion, releaseNotes } = require('./filesHelper');
+const { context } = require('@actions/github');
 // const fs = require('fs');
 
 // const createRelease = async () => {
@@ -73,6 +74,10 @@ const createVersionRelease = async (octo, version) => {
   const releaseName = `Release v${version}`;
   
   try {
+    console.log(`context ${context}`);
+    console.log(`context repo ${context.repo}`);
+    console.log(`Current owner ${context.repo.owner}`);
+    console.log(`Current owner ${currentOwner()}`);
     await createRelease(octo, currentOwner(), currentRepo(), 'main', version, releaseName, releaseNotes(CURR_CHANGELOG_PATH));
     // await createRelease(octo, version, releaseName, releaseNotes(CURR_CHANGELOG_PATH));
   } catch(error) {
